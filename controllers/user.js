7e6e82516b76cb65a19eca72335879158ddaf31e
adel <adel.g.moussa@hotmail.com^>
2023-10-23 13:39:46 +0200

    done

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const userController = {
+
+setCookiesAndSession: (req, res) => {
+    req.session.userName = 'AdelMoussa';
+    res.cookie('name', 'Adel', { maxAge: 5 * 60 * 1000 });
+    res.send('hello');
+},
+
+verifyCookiesAndSession:(req, res) => {
+    const { name } = req.cookies;
+    console.log(name);
+    console.log(req.session.userName);
+    if (req.session && req.session.userName && name) {
+        const userName = req.session.userName;
+        res.send(`hello ${name} your username is: ${userName}`);
+    } else {
+        res.send('session or cookie is not valid');
+    }
+}
+}
+
+
+export default userController;
